4db9542c24e3734c0ed1cf6b386b5ce94bd0d1fa
deepaklr <75823578+deepaklr@users.noreply.github.com>
2021-02-25 11:11:59 +0530

    Add files via upload

---
+++ dpTool-auth.ps1
@@ -0,0 +1,10 @@
+﻿#Enter your user id and password
+$userid="JOKER"
+$password="JOKER"
+
+$bytes = [System.Text.Encoding]::ASCII.GetBytes($userid + ":" + $password)
+$base64 = [System.Convert]::ToBase64String($bytes)
+$basicAuthValue = "Basic $base64"
+Write-Output $basicAuthValue
+
+
